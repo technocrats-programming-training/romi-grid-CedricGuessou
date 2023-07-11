7115fd7fc0557b43fe31532cfffae59b61148035
CedricGuessou <121789165+CedricGuessou@users.noreply.github.com>
2023-07-11 23:51:50 +0000

    First try

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -4,21 +4,30 @@ import static lib.Romi.driveDistance;
 import static lib.Romi.turnDegrees;
 
 public class Position {
+  Romi romiGrid = new Romi();
 
 
     public void driveUp() throws InterruptedException {
+      romiGrid.driveDistance();
+      romiGrid.turnDegrees();
 
     }
 
     public void driveDown() throws InterruptedException {
+      romiGrid.turnDegrees(180);
+      romiGrid.driveDistance();
 
     }
 
     public void driveRight() throws InterruptedException {
+      romiGrid.turnDegrees(-90);
+      romiGrid.driveDistance();
 
     }
 
     public void driveLeft() throws InterruptedException {
+      romiGrid.turnDegrees(90);
+      romiGrid.driveDistance();
 
     }
 
@@ -27,6 +36,6 @@ public class Position {
     }
 
     public String toString() {
-        return "";
+        return "Your current position is ";
     }
 }
